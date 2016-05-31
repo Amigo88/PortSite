bd316dcd925fc0f5439e78fbe03748cec2fc85bb
Amigo88 <amit.goldst@gmail.com>
2016-05-31 16:58:37 +0300

    added dispatch and valid form to LoginView

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout, authenticate
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.shortcuts import render, redirect
-from django.views.generic import FormView
-from . import forms
+from django.utils.encoding import escape_uri_path
+from django.views.generic import FormView, CreateView, ListView
+from . import forms, models
 from django.views.generic import View
 
 
@@ -9,8 +11,48 @@ class LoginView(FormView):
     form_class = forms.LoginForm
     template_name = "login.html"
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return redirect('profiles:list')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = authenticate(username=form.cleaned_data['username'],
+                            password=form.cleaned_data['password'])
+
+        if user is not None and user.is_active:
+            login(self.request, user)
+            if self.request.GET.get('from'):
+                return redirect(
+                    self.request.GET['from'])  # SECURITY: check path
+            return redirect('profiles:list')
+
+        form.add_error(None, "Invalid user name or password")
+        return self.form_invalid(form)
+
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect("login")
+
+
+class LoggedInMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            url = reverse("login") + "?from=" + escape_uri_path(request.path)
+            return redirect(url)
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ListProfilesView(LoggedInMixin, ListView):
+    page_title = "Profiles"
+    model = models.Profile
+    template_name = "profiles\profile_list.html"
+
+
+class CreateProfileView(LoggedInMixin, CreateView):
+    page_title = "Create New Profile"
+    model = models.Profile
+    form_class = forms.ProfileForm
+    template_name = "profiles\portfolio_form.html"
