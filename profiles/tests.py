e35253fea659425beef2c8c2cb2978e1e1715574
Amigo88 <amit.goldst@gmail.com>
2016-05-31 14:44:11 +0300

    updated test_profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,19 +2,17 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from . import models
 
+
 class ProfilesTestCase(TestCase):
     def test_portfolios(self):
-        n=10
-        users = [User.objects.create_user(
-            "user #{}".format(i+1)) for i in range(n)]
-
+        n = 10
         for i in range(n):
             o = models.Profile(
-                user=users[i],
-                first_name = "a",
-                last_name = "b",
-                contact_mail = "a@a.com",
-                short_description = "I am",
+                user=User.objects.create_user("user #{}".format(i + 1)),
+                first_name="a",
+                last_name="b",
+                contact_mail="a@a.com",
+                short_description="I am",
             )
             o.full_clean()
             o.save()
